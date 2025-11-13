4b2a9758d25ec092c32d5bf8b3a1a21a7e27a894
sr-sebastian <bruno.demello@estudiantes.utec.edu.uy>
2025-11-13 01:52:55 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // <-- 1. Importa el router
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  // --- INICIO DEL CAMBIO ---
+  // 2. Envuelve tu App con el BrowserRouter y el basename
+  <BrowserRouter basename="/mapa-lidia-2/">
+    <App />
+  </BrowserRouter>
+  // --- FIN DEL CAMBIO ---
+);
